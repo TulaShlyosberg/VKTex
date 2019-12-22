ba7aa4d173137930427e98a9744bf3b501a48c5a
LAPTOP-UUH16LKD\Fedor <fedor.noskov.99@mail.ru>
2019-12-22 12:46:37 +0300

    Первая формула прикреплена

--- js/injector.js
+++ js/injector.js
@@ -1,3 +1,4 @@
+// импортируем библиотеки
 libs = [
     'html2canvas.js',
     'jQuery.min.js',
@@ -8,7 +9,25 @@ libs = [
 for (let lib in libs) {
     let script = document.createElement('script');
     script.setAttribute("type", "text/javascript");
-    script.setAttribute("src", chrome.extension.getURL("js/" + lib));
+    script.setAttribute("src", chrome.extension.getURL("js/" + libs[lib]));
     let head = document.head || document.getElementsByTagName( "head" )[0] || document.documentElement;
     head.appendChild(script);
 }
+
+console.log('Scripts injected');
+
+// импортируем стили
+stylesheets = [
+    "katex.min.css",
+    "main.css"
+]
+
+for (url in stylesheets) {
+    let new_link = document.createElement("link");
+    new_link.setAttribute('rel', "stylesheet");
+    new_link.setAttribute('href', chrome.extension.getURL("css/" + stylesheets[url]));
+    new_link.setAttribute('crossorigin', "anonymous");
+    document.head.appendChild(new_link);
+}
+
+console.log('Css injected');
--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 (function() {
     'use strict';
-    appendExternalCSS();
     main();
 })();
-
-
-//классом rendered будем помечать блоки с отредеренными формулами
-function appendCSS(){
-    $('<style>\
-       .rendered {}\
-      </style>').appendTo($("html > head"));
-}
-
 
 //заменяем html символы на unicode
 function htmlReplacer(str, offset, s){
@@ -28,9 +18,15 @@ function formulaReplacer(str, group_1, group_2, offset, s){
     var formula, formulaType = str[0] == '\\', res;
     if (formulaType) formula = group_2;
     else formula = group_1;
+    //удалить потом класс формула
     var buffer = document.createElement('span');
+    buffer.className = 'formula';
     try{   //рендерим формулы и после возвращаем результат
-            katex.render(formula.replace(/&quot;|&gt;|&lt;|<br>|amp;/g, htmlReplacer), buffer, {displayMode: formulaType});
+            katex.render(
+                formula.replace(/&quot;|&gt;|&lt;|<br>|amp;/g, htmlReplacer),
+                buffer, 
+                {displayMode: formulaType}
+            );            
             res = buffer.innerHTML;
         } catch(e) {
             buffer.setAttribute('style', 'background: #fc0;');
@@ -52,23 +48,33 @@ function prepareInnerHTML(messIndex, innerStr){
 function render_all(){
     $(".im-mess:not(.rendered),\
        .reply_content:not(.rendered),\
-       .wall_post_text:not(.rendered),\
-       .article_layer__content:not(.rendered)").html(prepareInnerHTML);
+       .wall_post_text:not(.rendered)").html(prepareInnerHTML);
 }
 
 
+function dummy() {
+    buffer = $('.katex-display')[0]; 
+    buffer.style.padding = '13px';
+    buffer.style.height = '100px';
+    html2canvas(buffer, {
+        windowWidth: buffer.scrollWidth,
+        windowHeight: buffer.scrollHeight,
+        width: buffer.clientWidth,
+        height: $(buffer).outerHeight()
+    }).then(function(canvas) {
+        let context = canvas.getContext('2d');
+        context.scale(0.5, 0.5);
+        console.log(canvas.toDataURL());
+        canvas.toBlob(function(blob) {
+            blob.filename = 'formula12456.png';
+            blob.name = 'formula12456.png';
+            window.Upload.onFileApiSend(2, [blob]);
+        });
+    });
+}
 
 function main(){
-    appendCSS();
     setInterval(render_all, 200);
+    setTimeout(dummy, 10000)
 }
 
-//подключаем внешние стили
-function appendExternalCSS(){
-		var new_link = document.createElement("link");
-		new_link.setAttribute('rel', "stylesheet");
-		new_link.setAttribute('href', "https://cdn.jsdelivr.net/npm/katex@0.10.0-beta/dist/katex.min.css");
-		new_link.setAttribute('integrity', "sha384-9tPv11A+glH/on/wEu99NVwDPwkMQESOocs/ZGXPoIiLE8MU/qkqUcZ3zzL+6DuH");
-		new_link.setAttribute('crossorigin', "anonymous");
-		document.head.appendChild(new_link);
-	}
